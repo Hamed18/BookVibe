bb366ec8c5d8954ddfef9b4b019e4a4d5cb3f4f3
hamed18 <hamedcnec@gmail.com>
2024-05-23 23:32:45 +0600

    Starting Layout Setup: Home Page

---
+++ src/Components/Books/Books.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Books = () => {
+	return (
+		<div>
+			<h3 className="text-center font-bold my-8">Books</h3>
+		</div>
+	);
+};
+
+export default Books;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import Books from "../Books/Books";
 
 const Home = () => {
-	return (
-		<div>
-			<h3 className="text-center">Home</h3>
-		</div>
-	);
+  return (
+    <div>
+
+      <div className="card card-side bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Books to freshen <br /> up your bookshelf</h2>
+          <div className="card-actions justify-start">
+            <button className="btn btn-primary">View The List</button>
+          </div>
+        </div>
+
+		<figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+            alt="Movie"
+          />
+        </figure>
+      </div>
+
+	  <Books></Books>
+
+    </div>
+  );
 };
 
 export default Home;
