78476dd3e84238a6da27b0aad3c68142ebc43128
hamed18 <hamedcnec@gmail.com>
2024-05-24 00:05:51 +0600

    Books data fetched and loaded

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Books = () => {
+	const [books,setBooks] = useState([]);
+
+	useEffect( () => {
+		fetch('books.json')
+		.then(res => res.json())
+		.then(data => setBooks(data))
+	},[])
+	console.log(books);
+
 	return (
 		<div>
-			<h3 className="text-center font-bold my-8">Books</h3>
+			<h3 className="text-center font-bold my-8">Books: {books.length}</h3>
 		</div>
 	);
 };
 
 export default Books;
+
+
+
+// Books section lies in Home Page.
